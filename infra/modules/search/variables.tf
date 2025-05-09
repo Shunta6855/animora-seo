381e7690a67fd98dad4a47c2e6dba48049d60e43
Shunta6855 <shuntakochi@gmail.com>
2025-05-09 13:35:51 +0000

    feat: add search function

--- infra/modules/search/variables.tf
+++ infra/modules/search/variables.tf
@@ -1,9 +1,28 @@
+# ---------------------------------------------------------------------------------  # 
+#                                       変数の定義                                     #
+# ---------------------------------------------------------------------------------  #
+
+# プロジェクト名
 variable "project_name" {
-  description = "プロジェクト名"
-  type        = string
+    description = "Name of the project"
+    type = string
 }
 
+# Azureのリージョン(ex: japaneast)
 variable "location" {
-  description = "Azure上のリージョン"
+    description = "Azure region"
+    type = string
+}
+
+# リソースグループ名
+variable "resource_group_name" {
+  description = "Resource group name"
   type        = string
 } 
+
+# Azure AI Search ServiceのSKU
+variable "search_sku" {
+    description = "Azure Cognitive Search pricing tier"
+    type = string
+    default = "basic"
+}
